10c7862498e34a037f39f1d6fdf8ade7c9c152c5
Do Thi Hai <dothihaia10k50@gmail.com>
2018-08-14 08:00:31 +0000

    micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810165118) do
+ActiveRecord::Schema.define(version: 20180814073438) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
