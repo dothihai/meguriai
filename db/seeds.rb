cb09aca91f279204be641e8d8629d10c57f67c9a
森田源太郎 <unagi1234@icloud.com>
2018-08-14 09:49:59 +0000

    userhobbies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,31 @@ Hobby.new(
     title: "音楽",
     description: "音楽を聴く",
   ).save
+Hobby.new(
+    title: "水泳",
+    description: "泳ぐ",
+  ).save
+Hobby.new(
+    title: "野球",
+    description: "試合観戦",
+  ).save
+Hobby.new(
+    title: "バスケットボール",
+    description: "試合観戦",
+  ).save
+Hobby.new(
+    title: "テニス",
+    description: "no data",
+  ).save
+Hobby.new(
+    title: "スキー",
+    description: "no data",
+  ).save
+Hobby.new(
+    title: "釣り",
+    description: "魚を釣る",
+  ).save
+Hobby.new(
+    title: "書道",
+    description: "no data",
+  ).save
